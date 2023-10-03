3b7d8515cf336e8f6a314fdd6a621c889fac8ae9
Julien Blin <julien@alleycorpnord.com>
2023-10-03 12:34:03 -0400

    Add user info at the top.

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,4 +1,6 @@
-import { Group, Header } from "@mantine/core";
+import { Avatar, Button, Group, Header, Menu } from "@mantine/core";
+import { IconChevronDown, IconLogin, IconLogout } from "@tabler/icons-react";
+import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import logo from "./logo.svg";
 
@@ -13,7 +15,58 @@ export default function AppHeader() {
     >
       <Group position="apart" sx={{ height: "100%" }}>
         <Image src={logo} alt="logo" priority={true} />
+        <UserMenu />
       </Group>
     </Header>
   );
+}
+
+function UserMenu() {
+  const { data: session, status } = useSession();
+
+  if (status !== "authenticated") {
+    return (
+      <Button
+        onClick={() => signIn("medplum")}
+        size="xs"
+        leftIcon={<IconLogin size="1rem" />}
+      >
+        Login
+      </Button>
+    );
+  }
+
+  return (
+    <Menu shadow="md" width={200}>
+      <Menu.Target>
+        <Button
+          size="xs"
+          leftIcon={
+            <Avatar size="sm">
+              {initials(session.user?.profile?.display)}
+            </Avatar>
+          }
+          rightIcon={<IconChevronDown size="1rem" />}
+        >
+          {session.user?.profile?.display}
+        </Button>
+      </Menu.Target>
+      <Menu.Dropdown>
+        <Menu.Item
+          onClick={() => signOut({ callbackUrl: "/" })}
+          icon={<IconLogout size={14} />}
+        >
+          Logout
+        </Menu.Item>
+      </Menu.Dropdown>
+    </Menu>
+  );
+}
+
+function initials(name: string | undefined) {
+  return name
+    ?.split(" ")
+    .map((n) => n[0])
+    .slice(0, 2)
+    .join("");
 }
