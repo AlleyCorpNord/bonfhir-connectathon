774dcb26778e92eaba2200adb93e1c8e9ea1d8fe
Julien Blin <julien@alleycorpnord.com>
2023-11-15 14:24:12 -0500

    Update to support ACN Medplum instance

--- src/config.tsx
+++ src/config.tsx
@@ -1,20 +1,29 @@
 export const Config = {
   public: {
     fhirUrl:
-      process.env.NEXT_PUBLIC_FHIR_URL || "http://localhost:8103/fhir/R4/",
+      process.env.NEXT_PUBLIC_FHIR_URL ||
+      "https://api.medplum.alleycorpnord.com/fhir/R4/",
   },
   server: {
-    authServerUrl: process.env.AUTH_SERVER_URL || "http://localhost:8103",
+    authServerUrl:
+      process.env.AUTH_SERVER_URL || "https://api.medplum.alleycorpnord.com",
     authTokenUrl:
-      process.env.AUTH_TOKEN_URL || "http://localhost:8103/oauth2/token",
-    logoutUrl: process.env.LOGOUT_URL || "http://localhost:8103/oauth2/logout",
+      process.env.AUTH_TOKEN_URL ||
+      "https://api.medplum.alleycorpnord.com/oauth2/token",
+    logoutUrl:
+      process.env.LOGOUT_URL ||
+      "https://api.medplum.alleycorpnord.com/oauth2/logout",
     authClientId:
-      process.env.AUTH_CLIENT_ID || "f54370de-eaf3-4d81-a17e-24860f667912",
+      process.env.AUTH_CLIENT_ID || "4676be68-5f51-4056-b3da-3229b49d4292",
     authClientSecret:
       process.env.AUTH_CLIENT_SECRET ||
-      "75d8e7d06bf9283926c51d5f461295ccf0b69128e983b6ecdd5a9c07506895de",
+      "36bf0707b0c8e553ccc3e8e9e30d8b0b36ea76e7e5cb6c4f26414eb41fcd5271",
     authSecret: process.env.AUTH_SECRET || "secret",
-    appBaseUrl: process.env.APP_BASE_URL || "http://host.docker.internal:3000",
+    appBaseUrl:
+      process.env.APP_BASE_URL ||
+      (process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}`
+        : "http://host.docker.internal:3000"),
     fhirSubscriptionsSecret: process.env.FHIR_SUBSCRIPTION_SECRET || "secret",
   },
 } as const;
